00b79418d67220f3e93c3619d24335ab66828d37
Jose Alberto de la Paz <30326968+AlbertodelaPaz@users.noreply.github.com>
2019-11-14 22:18:22 +0100

    Delete GetItDone.m

--- GetItDone.m
+++
@@ -1,8 +0,0 @@
-function GetItDone(path_data)
-	evolveFolder(path_data,30000);
-	HeterotachyFolder(path_data,10000);
-	GammaFolder(path_data);
-	Heterotachy_figures(path_data);
-	stokesFolder(path_data);
-	Single_fam_alpha(path_data);
-end
