8b413bcf69ba5f215fd4866dd6a93ba08904ac5f
Jose Alberto de la Paz <30326968+AlbertodelaPaz@users.noreply.github.com>
2019-11-15 00:39:40 +0100

    Update Rates.m

--- Rates.m
+++ Rates.m
@@ -1,4 +1,26 @@
 function [sites,TN,TS,r]=Rates(Syn,Non_Syn,sitecount,sustcount)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%	Inference of the individual site rates. Input generated with RatesEvolution.m
+%	INPUT:
+%       sitecount          -Number of times a single site was sampled along the
+%                           simulation.
+%       sustcount          -Number of non-synonymous substitution every site
+%                           undergoes along the simulation
+%		Non_syn            -Matrix of step-stamps (M x L) marking the step and site
+%                           where a non-synounymous substitution happens.
+%       Syn                -Matrix of step-stamps (M x L) marking the step and site
+%                           where a synounymous substitution happens.
+%	OUTPUTS:
+%		r                  -Empirical mutation rate for sites with sitecount > 1
+%                           and sustcount > 1
+%       TS                 -Recurrence intervals for non-synonymous substituion
+%                           for each site
+%       TN                 -Recurrence intervals for sampling each site
+%       sites              -Index for the sites with sitecount > 1 and sustcount > 1
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     sites=find((sitecount>1)&(sustcount>1));
     Syn=Syn(:,sites);
     Non_Syn=Non_Syn(:,sites);
