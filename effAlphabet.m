1d58e6d109a50ea134fd6365a2dad671ffd56700
Jose Alberto de la Paz <30326968+AlbertodelaPaz@users.noreply.github.com>
2019-11-15 01:08:57 +0100

    Update effAlphabet.m

--- effAlphabet.m
+++ effAlphabet.m
@@ -1,5 +1,22 @@
 function [expS,aver]=effAlphabet(Trajectory,e,h)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%	Effective alphabet measure from the single-site probability distribution along trajectory
+%	INPUT:
+%		Trjectory  - Matrix of evolve sequences (M x L), every row corresponding
+%                    to a different evolutionary step.
+%		e          - Coupling matrix (qLxqL) formed from L x L submatrices
+%			         of size q x q with the pairwise couplings for each pair 
+%			         of sites (L submatrices in diagonal are irrelevant).
+%		h          - h fields matrix (q x L) with the single site contributions
+%			         to the Hamiltonian.
+%
+%	OUTPUTS:
+%		expS       - Effective alphabet for every site along the trajectory
+%		aver       - Average effective alphabet along the sites for every step.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 [M,L]=size(Trajectory);
 expS=zeros(M,L);
 aver=zeros(M,1);
